9fd376972da627c2d47a9af2084618af60d6eff6
dgk3593 <62585895+dgk3593@users.noreply.github.com>
2020-04-19 16:36:57 +0700

    Adding special adv

--- adv_obj_gen.py
+++ adv_obj_gen.py
@@ -1,5 +1,7 @@
 import json
 
+sp_adv = ['100002_013', '100029_02', '100029_03', '100010_04', '110328_01', '110007_02', '100001_08', '100003_07', '110291_01', '110327_01', '110050_02', '100005_02', '100004_10', '110333_01', '110354_01', '100006_09']
+
 f = open('adv_dict.txt','r')
 data = f.read()
 f.close()
@@ -7,6 +9,9 @@ adv_dict = eval(data)
 
 f = open('adv_list.js','w')
 f.write("const adv = [\n")
+
+s = open('sp_adv.js','w')
+s.write("const sp_adv = [\n")
 
 comma = False
 
@@ -25,6 +30,17 @@ for entry in adv_dict:
     f.write('\t\trarity: ' + adv["rarity"] + '\n')
     f.write('\t}')
 
+    if entry in sp_adv:
+        s.write('\t{\n')
+        s.write('\t\tcid: \'' + entry + '\',\n')
+        s.write('\t\tname: \'' + adv["name"].replace('\'s','') + '\',\n')
+        s.write('\t\tweapon: \'' + adv["weapon"] + '\',\n')
+        s.write('\t}')
+
 f.write('\n];\n')
 f.write('export default adv;')
 f.close()
+
+s.write('\n];\n')
+s.write('export default sp_adv;')
+s.close()
---
+++ js/sp_adv.js
@@ -0,0 +1,64 @@
+const sp_adv = [
+	{
+		cid: '110050_02',
+		name: 'Hunter Berserker',
+		weapon: 'Sword',
+	}	{
+		cid: '110291_01',
+		name: 'Marth',
+		weapon: 'Sword',
+	}	{
+		cid: '100010_04',
+		name: 'Gala Mym',
+		weapon: 'Lance',
+	}	{
+		cid: '100029_02',
+		name: 'Gala Sarisse',
+		weapon: 'Bow',
+	}	{
+		cid: '110354_01',
+		name: 'Mega Man',
+		weapon: 'Wand',
+	}	{
+		cid: '100002_013',
+		name: 'Gala Elisanne',
+		weapon: 'Axe',
+	}	{
+		cid: '110328_01',
+		name: 'Fjorm',
+		weapon: 'Lance',
+	}	{
+		cid: '100029_03',
+		name: 'Hunter Sarisse',
+		weapon: 'Bow',
+	}	{
+		cid: '100003_07',
+		name: 'Gala Ranzal',
+		weapon: 'Sword',
+	}	{
+		cid: '100001_08',
+		name: 'Gala Prince',
+		weapon: 'Sword',
+	}	{
+		cid: '110327_01',
+		name: 'Alfonse',
+		weapon: 'Sword',
+	}	{
+		cid: '100006_09',
+		name: 'Gala Luca',
+		weapon: 'Blade',
+	}	{
+		cid: '110007_02',
+		name: 'Hunter Vanessa',
+		weapon: 'Lance',
+	}	{
+		cid: '100005_02',
+		name: 'Gala Alex',
+		weapon: 'Sword',
+	}	{
+		cid: '110333_01',
+		name: 'Veronica',
+		weapon: 'Wand',
+	}
+];
+export default sp_adv;
