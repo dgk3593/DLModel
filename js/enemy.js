42140fd15090808d3e28ab7c4600f3c897f132bd
dgk3593 <62585895+dgk3593@users.noreply.github.com>
2020-04-20 11:14:11 +0700

    New files

---
+++ js/enemy.js
@@ -0,0 +1,19 @@
+const enemy = [
+	{
+		cid: '100016_01',
+		name: 'Valyx',
+	},
+	{
+		cid: '100032_02',
+		name: 'Mad Laxi',
+	},
+	{
+		cid: '100033_01',
+		name: 'Volk',
+	},
+	{
+		cid: '100034_01',
+		name: 'Kai Yan',
+	}
+];
+export default enemy;
